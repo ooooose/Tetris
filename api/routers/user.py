6b9c115ab2e40bed577529b0a64e3138a150e2ef
ooooose <yuuki.oose1990@gmail.com>
2023-10-17 11:07:29 +0900

    ADD 必要なエンドポイント作成

--- api/routers/user.py
+++ api/routers/user.py
@@ -10,6 +10,26 @@ from setting import get_db
 
 router = APIRouter()
 
-@router.get('/ranking')
-async def get_ranking():
-    return {"message": "ranking!!"}
+@router.get('/ranking', response_model=List[UserOrm])
+async def get_ranking(db: Session = Depends(get_db)):
+    """
+    ランキングを取得するエンドポイント
+    （現在は、User一覧を取得するエンドポイントとして暫定的に作成している）
+    """
+
+    users = db.scalars(select(User)).all()
+    return users
+
+
+@router.post('/users', response_model=UserOrm)
+async def create_user(user_data: UserCreate, db: Session = Depends(get_db)):
+    """
+    Userを一件分作成するためのエンドポイント
+    """
+
+    user = User(**user_data.dict())
+    db.add(user)
+    db.commit()
+    db.refresh(user)
+
+    return user
