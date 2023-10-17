283c1569be73b9438be047327d837a243da28dc1
ooooose <yuuki.oose1990@gmail.com>
2023-10-17 10:52:27 +0900

    ADD CORS設定

--- api/main.py
+++ api/main.py
@@ -3,6 +3,19 @@ from routers import user
 
 app = FastAPI()
 
+//TODO: URLは環境変数として管理するよう修正
+origins = [
+  "https://localhost:8000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_creditionals=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
 app.include_router(user.router)
 
 @app.get("/hello")
