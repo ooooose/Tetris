87641d8135c12f14c9d65ef08b4236abc96ded05
ooooose <yuuki.oose1990@gmail.com>
2023-10-18 07:27:08 +0900

    ADD ページにて疎通確認

--- front/src/pages/index.tsx
+++ front/src/pages/index.tsx
@@ -1,8 +1,24 @@
+import { useGetUsers } from "@/stores/useUsers/getUsers"
+
 
 export default function Home() {
+  const { data: users } = useGetUsers();
+  if (!users) return <div>Loading...</div>;
   return (
     <>
       <h1>Hello Tetris!</h1>
+      
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.name}</p>
+              <br />
+            </div>
+          )
+        })}
+
+      </div>
     </>
   )
 }
