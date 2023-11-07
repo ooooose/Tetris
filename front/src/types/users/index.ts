43aaaf3bee018f99cfee47e3c8239a820d6d8ea8
ooooose <yuuki.oose1990@gmail.com>
2023-11-08 05:41:30 +0900

    Add: Userの型を定義

--- front/src/types/users/index.ts
+++ front/src/types/users/index.ts
@@ -1,5 +1,20 @@
-export type User = {
-  id: number;
+export interface UserInfo {
   name: string;
   score: number;
-};
+}
+
+export interface User {
+  name: string;
+  email: string;
+  password: string;
+}
+
+export interface AuthUser {
+  email: string;
+  password: string;
+}
+
+export interface CsrfToken {
+  csrf_token: string;
+}
+
