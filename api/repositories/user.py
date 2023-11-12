90cf4d4bdefba52b00080ad9515bc87008eb481f
ooooose <yuuki.oose1990@gmail.com>
2023-11-12 21:59:21 +0900

    login機能の実装

--- api/repositories/user.py
+++ api/repositories/user.py
@@ -1,5 +1,7 @@
 from fastapi import HTTPException
-from schemas.user import User, UserOrm
+from typing import Union
+from models.user import User
+from schemas.user import UserOrm, LoginUser
 from sqlalchemy import select
 from sqlalchemy.orm import Session
 
@@ -7,14 +9,14 @@ class UserRepository:
     def __init__(self, session: Session):
         self.session = session
 
-    def find_user_by_email(self, email: str) -> UserOrm:
+    def find_user_by_email(self, email: str) -> Union[UserOrm, None]:
         """
         emailと一致するUserモデルのインスタンスを返す
         """
-        user = self.session.scalar(select(User).where(User.email == email))
-        if user is None:
-            raise HTTPException(status_code=404, detail="User not found")
-
+        try:
+            user = self.session.scalar(select(User).where(User.email == email))
+        except ValueError:
+            user = None
         return user
 
     def find_user_by_id(self, id: int) -> UserOrm:
@@ -27,11 +29,12 @@ class UserRepository:
 
         return user
 
-    def insert_user(self, user_data: User) -> UserOrm:
+    def insert_user(self, user_data: dict) -> UserOrm:
         """
         userを登録
         """
-        user = User(**user_data.dict())
+        print(user_data)
+        user = User(**user_data)
         self.session.add(user)
         self.session.commit()
         self.session.refresh(user)
