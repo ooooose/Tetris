95925cd475952efa37a999d0782f86eadb5532b9
ooooose <yuuki.oose1990@gmail.com>
2023-12-03 07:09:50 +0900

    faviconの設定

--- front/src/pages/_document.tsx
+++ front/src/pages/_document.tsx
@@ -4,7 +4,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="ja">
-      <Head />
+      <Head>
+      <link rel="icon" href="/icon.svg" />
+      </Head>
       <body>
         <Main />
         <NextScript />
